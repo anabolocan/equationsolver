911a45a32d83727ab363921d4502dfcb98b913b0
anabolocan <rihannaledi@mail.ru>
2021-05-24 21:05:37 +0300

    initial commit

---
+++ src/main/java/com/math/equationsolver/EquationSolverApplication.java
@@ -0,0 +1,13 @@
+package com.math.equationsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquationSolverApplication.class, args);
+	}
+
+}
